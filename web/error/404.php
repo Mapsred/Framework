34bdd13bb81b1d969874b9abd4a173c033c6e89a
François.Mathieu <root@francoismathieu.ovh>
2016-02-10 10:29:58 +0100

    404 text edit

--- web/error/404.php
+++ web/error/404.php
@@ -6,4 +6,12 @@
  * Time: 11:27
  */
 
-echo "La page demandée n'existe pas ou n'existe plus";
+//echo "La page demandée n'existe pas ou n'existe plus";
+
+
+echo "<h2>Désolé, la page que vous demandez est introuvable.</h2>";
+echo "<p><i>Oupsss…. page introuvable!
+
+La page que vous avez demandée n’a pas été trouvée. Il se peut que  le lien que vous avez utilisé n'existe plus
+ou que vous ayez tapé l’adresse (URL) incorrectement. </i></p>";
+
